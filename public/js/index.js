5ef6103be260f33da6790b78db498a6fcdcfaba5
Victor Nhem <nhemvictor@gmail.com>
2018-08-19 08:52:53 -0500

    Add newMessage and createMessage event

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+let socket = io();
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'Victor',
+    text: 'this is victor'
+  });
+});
+socket.on('disconnect', function () {
+  console.log('Disconnnected from server');
+});
+
+socket.on('newMessage', function(message) {
+  console.log('newMessage', message);
+});
